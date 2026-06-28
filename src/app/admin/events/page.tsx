0cab2af2e2b6c0ea5895ef48dcb68a80e0c7b3ad
handi <aviaryparks@gmail.com>
2026-06-28 21:53:06 +0700

    Add missing toast import to events page

--- src/app/admin/events/page.tsx
+++ src/app/admin/events/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 import { supabase } from '@/lib/supabase';
 
 export default function EventsAdminPage() {
