6165b5bfd6cdd0a6433ddb79cb7c8e726d697748
handi <aviaryparks@gmail.com>
2026-06-29 11:59:51 +0700

    Remove blur overlay from registration page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -393,16 +393,7 @@ export default function Register() {
         backgroundPosition: 'center',
         opacity: 1,
       }}></div>
-      <div style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        zIndex: 0,
-        backgroundColor: 'rgba(255,255,255,0.7)',
-        backdropFilter: 'blur(2px)'
-      }}></div>
+
 
 
 
